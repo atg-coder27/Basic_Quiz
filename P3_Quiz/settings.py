0bd4aabd3b97918e8c932d7788766ac1d97e707c
atg-coder27 <72323013+atg-coder27@users.noreply.github.com>
2021-06-08 13:52:50 +0530

    Commit

--- P3_Quiz/settings.py
+++ P3_Quiz/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+from decouple import config
+import dj_database_url
 
 from pathlib import Path
 
@@ -21,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xxsu!dxve(qx5#+7%ff=)^ij+nqu1rd*(w0+^%xj@npt+b3pb='
+#SECRET_KEY = 'django-insecure-xxsu!dxve(qx5#+7%ff=)^ij+nqu1rd*(w0+^%xj@npt+b3pb='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -81,11 +85,16 @@ WSGI_APPLICATION = 'P3_Quiz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
